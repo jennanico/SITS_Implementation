ba0b6cad41888561326782091abe6f5bf92462ac
Jenna <jenna.nicospace@gmail.com>
2026-03-01 13:30:54 -0500

    Logger functionality and tests

---
+++ src/main/java/sits/ResultLogger.java
@@ -0,0 +1,37 @@
+package sits;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ResultLogger extends Observer {
+
+	@Override
+	void update() {
+		Game gameSubject = (Game) subject;
+		
+		try (FileWriter file = new FileWriter("gameResultsLog.txt", true)) {
+			
+			if (gameSubject.endGame()) {
+				
+				file.write("\n\nGame Results - P1: " + gameSubject.currState.p1Name + " vs. P2: " + gameSubject.currState.p2Name);
+				file.write("\nP1 Score: " + gameSubject.currState.p1Score + " | P2 score: " + gameSubject.currState.p2Score);
+				
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error!");
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void clearCache() {
+		try (FileWriter file = new FileWriter("gameResultsLog.txt")) 
+		{
+		} catch (IOException e) {
+			System.out.println("Error!");
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/test/java/sits/LoggingSystemTest.java
+++ src/test/java/sits/LoggingSystemTest.java
@@ -9,45 +9,61 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.assertj.core.util.Files;
+import org.codehaus.plexus.util.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LoggingSystemTest {
 	
 	IteratedPrisonersDilemma IPD = new IteratedPrisonersDilemma(5);
-	ActionLogger logger = new ActionLogger();
+	
+	ActionLogger gameLogger = new ActionLogger();
+	ResultLogger resultLogger = new ResultLogger();
+	
 	SelfishBot bot1 = new SelfishBot("1");
 	SelflessBot bot2 = new SelflessBot("2");
 
 	@BeforeEach
 	void setUp() throws Exception {		
-		IPD.register(logger);
+		IPD.register(gameLogger);
+		IPD.register(resultLogger);
 	}
 
 	@Test
 	void testRegisterListener() {
-		fail("Not yet implemented");
+		assertTrue(IPD.listeners.contains(gameLogger));
+		assertTrue(IPD.listeners.contains(resultLogger));
+
 	}
 	
 	@Test
 	void testDeregisterListener() {
-		fail("Not yet implemented");
+		IPD.deregister(gameLogger);
+		IPD.deregister(resultLogger);
+		assertFalse(IPD.listeners.contains(gameLogger));
+		assertFalse(IPD.listeners.contains(resultLogger));
 	}
 	
 	@Test
 	void testClearCache() {
-		logger.clearCache();
+		gameLogger.clearCache();
+		resultLogger.clearCache();
 		
 		File file = new File("gameActionsLog.txt");
+		File file2 = new File("gameResultsLog.txt");
 		assertTrue(file.exists());
+		assertTrue(file2.exists());
 		
 		String fileContent = Files.contentOf(file, StandardCharsets.UTF_8);
 		assertTrue(fileContent.contains(""));
+
+		String fileContent2 = Files.contentOf(file2, StandardCharsets.UTF_8);
+		assertTrue(fileContent2.contains(""));
 	}
 	
 	@Test
 	void testUpdateActionLogger() {
-		logger.clearCache();
+		gameLogger.clearCache();
 		
 		IPD.play(bot1, bot2);
 		
@@ -59,6 +75,47 @@ class LoggingSystemTest {
 		assertTrue(fileContent.contains("P1: 1 vs. P2: 2"));
 		assertTrue(fileContent.contains("0 | 1"));		
 	}
-
+	
+	@Test
+	void testUpdateResultLogger() {
+		resultLogger.clearCache();
+		
+		IPD.play(bot1, bot2);
+		
+		File file = new File("gameResultsLog.txt");
+		assertTrue(file.exists());
+		
+		String fileContent = Files.contentOf(file, StandardCharsets.UTF_8);
+		
+		assertTrue(fileContent.contains("Game Results"));
+		assertTrue(fileContent.contains("P1 Score: 0 | P2 score: -15"));
+	}
+	
+	@Test
+	void testTournamentLogging() {
+		gameLogger.clearCache();
+		resultLogger.clearCache();
+		
+		RoundRobinBracket roundrobin = new RoundRobinBracket();
+		Tournament tourney = new Tournament(IPD, roundrobin);
+		tourney.registerPlayer(new SelfishBot("1"));
+		tourney.registerPlayer(new SelfishBot("2"));
+		tourney.registerPlayer(new SelfishBot("3"));
+		tourney.playTournament();
+		
+		File file = new File("gameActionsLog.txt");
+		File file2 = new File("gameResultsLog.txt");
+		assertTrue(file.exists());
+		assertTrue(file2.exists());
+		
+		String fileContent = Files.contentOf(file, StandardCharsets.UTF_8);
+		int count = StringUtils.countMatches(fileContent, "vs");
+		assertEquals(3, count);
+		
+		String fileContent2 = Files.contentOf(file2, StandardCharsets.UTF_8);
+		count = StringUtils.countMatches(fileContent2, "Results");
+		assertEquals(3, count);
+	}
+	
 
 }
